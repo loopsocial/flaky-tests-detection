8f0e501e29910e5083e20e70c25f4887e8c9fbf1
xdays <easedays@gmail.com>
2024-12-09 17:59:18 +0800

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python /check_flakes.py $* | grep -A 5 'Top 5 flaky tests' > ./top-flaky-tests.txt
+python /check_flakes.py $* | grep -A 5 'Top 5 flaky tests' | tee ./top-flaky-tests.txt
 # https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#multiline-strings
-echo "report<<EOF"$'\n'"$(cat ./top-flake-test.txt)"$'\n'EOF >> "$GITHUB_OUTPUT"
+echo "report<<EOF"$'\n'"$(cat ./top-flaky-tests.txt)"$'\n'EOF >> "$GITHUB_OUTPUT"
